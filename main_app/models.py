f54e1bc25e9d1fe10e4547ad25355484713f4dd9
Emmett Verba <emmettverba@gmail.com>
2023-12-21 17:54:43 -0700

    completes lab pt 4

--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,27 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'recipe_id': self.id})
+    
+    def avg_rating(self):
+        total_rating = 0
+        for comment in self.comment_set.all():
+            total_rating += comment.rating
+        avg = total_rating/self.comment_set.all().count()
+        return avg
+        
+    
+
+class Comment(models.Model):
+    date = models.DateField()
+    rating = models.IntegerField()
+    comment = models.TextField(max_length=300)
+
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.comment} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from datetime import date
 from .models import Recipe
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -18,8 +20,10 @@ def recipes_index(request):
 
 def recipes_detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
+    comment_form = CommentForm()
     return render(request, 'recipes/detail.html', {
-        'recipe': recipe
+        'recipe': recipe,
+        'comment_form': comment_form
     })
 
 class RecipeCreate(CreateView):
@@ -33,4 +37,13 @@ class RecipeUpdate(UpdateView):
 class RecipeDelete(DeleteView):
     model = Recipe
     success_url = '/recipes'
+
+def add_comment(request, recipe_id):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.recipe_id = recipe_id
+        new_comment.date = date.today()
+        new_comment.save()
+    return redirect('detail', recipe_id=recipe_id)
 
